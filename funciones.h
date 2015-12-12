151c9215d623c23b3259f1ff43b8a845c214d210
Carlos Bulnes <carlos0bulnes@gmail.com>
2015-12-11 22:55:43 -0600

    nuevas funciones para manejo del bash

--- funciones.h
+++ funciones.h
@@ -14,7 +14,7 @@ void stdin_stdout(char *comando);
 int txt_to_txt(char *nombre_archivo_1, char *nombre_archivo_2, char *comando);
 int txt_to_stdout(char *nombre_archivo, char *comando);
 int stdin_to_txt(char *nombre_archivo, char *comando);
-void pipe_to_stdout(int pipe_in, char *comando);
+int pipe_to_stdout(int pipe_in, char *comando);
 int pipe_to_txt(int pipe_in, char *nombre_archivo, char *comando);
 int pipe_to_pipe(int pipe_in, char *comando);
 
@@ -76,7 +76,7 @@ int stdin_to_pipe(char *comando){
 
 void stdin_stdout(char *comando){
 	if(fork() == 0){
-		execl(SH_DIR, SH_DIR, "-c", comando);
+		execl(SH_DIR, SH_DIR, "-c", comando, 0);
 		_exit(EXIT_SUCCESS);
 	}
 	else{
@@ -102,12 +102,12 @@ int txt_to_txt(char *nombre_archivo_1, char *nombre_archivo_2, char *comando){
 
 	if(fork() == 0){
 		close(0); // Cierro stdin
-		close(1); // Cierro stdout
-
 		dup(archivo1); // Asigno archivo 1 a la entrada
+
+		close(1); // Cierro stdout
 		dup(archivo2); // Asigno archivo 2 a la salida
 
-		execl(SH_DIR, SH_DIR, "-c", comando);
+		execl(SH_DIR, SH_DIR, "-c", comando, 0);
 		_exit(EXIT_SUCCESS);
 	}
 	else{
@@ -153,22 +153,104 @@ int stdin_to_txt(char *nombre_archivo, char *comando){
 		return -1;
 	}
 
-	//if(fork() == 0){
-		// PENDIENTE
-	//}
+	if(fork() == 0){
+		close(1); // Cierra stdout
+		dup(archivo); // Asigna la salida al archivo
+
+		close(2);
+		dup(archivo);
+
+		execl(SH_DIR, SH_DIR, "-c", comando, 0);
+
+		_exit(EXIT_SUCCESS);
+	}
+	else{
+		wait(NULL);
+
+		close(archivo);
+		return EXIT_SUCCESS;
+	}
 }
 
-void pipe_to_stdout(int pipe_in, char *comando){
+int pipe_to_stdout(int pipe_in, char *comando){
 	printf("pipe_to_stdout\n");
+
+	if(pipe_in < 3){
+		printf("El descriptor de la tuberia no es valido\n");
+		return -1;
+	}
+
+	if(fork() == 0){
+		close(0); // Cierrra stdin
+		dup(pipe_in); // redirecciona stdin con el contenido de la tuberia
+
+		execl(SH_DIR, SH_DIR, "-c", comando, 0);
+		_exit(EXIT_SUCCESS);
+	}
+	else{
+		wait(NULL);
+
+		close(pipe_in);
+
+		return EXIT_SUCCESS;
+	}
 }
 
 int pipe_to_txt(int pipe_in, char *nombre_archivo, char *comando){
 	printf("pipe_to_txt\n");
+
+	if(pipe_in < 3){
+		printf("Descriptor de la tuberia no valido\n");
+		return -1;
+	}
+
+	int archivo = open(nombre_archivo, O_CREAT|O_TRUNC|O_WRONLY);
+
+	if(fork() == 0){
+		close(0);
+		dup(pipe_in);
+
+		close(1);
+		dup(archivo);
+
+		execl(SH_DIR, SH_DIR, "-c", comando, 0);
+		_exit(EXIT_SUCCESS);
+	}
+	else{
+		wait(NULL);
+
+		close(pipe_in);
+		close(archivo);
+
+		return EXIT_SUCCESS;
+	}
 }
 
 int pipe_to_pipe(int pipe_in, char *comando){
 	int p[2];
 	pipe(p);
 
-	return p[1];
+	if(pipe_in < 3){
+		printf("Descriptor de tuberia invalido\n");
+		return -1;
+	}
+
+	if(fork() == 0){
+		close(p[0]); // Cierra entrada de tuberia recien creada
+		close(0); // Cierra stdin
+		dup(pipe_in); // Lo remplaza por el pipe de entrada
+
+		close(1); // Cierra stdout
+		dup(p[1]); // Lo asigna a la salida de la tuberia recien creada
+
+		execl(SH_DIR, SH_DIR, "-c", comando, 0);
+		_exit(EXIT_SUCCESS);
+	}
+	else{
+		wait(NULL);
+
+		close(pipe_in);
+
+		return p[1];
+	}
 }
